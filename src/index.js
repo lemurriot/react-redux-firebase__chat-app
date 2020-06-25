bbbb69e1d8c8e7c2421ed14d4e5c781b099952fb
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-06-25 11:53:09 -0700

    wire up redux functionality for signing out user

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { createStore } from 'redux';
 import { Provider, connect } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
-import { setUser } from './actions';
+import { setUser, clearUser } from './actions';
 
 
 const store = createStore(rootReducer, composeWithDevTools());
@@ -25,6 +25,9 @@ class Root extends Component {
       if (user) {
         this.props.setUser(user);
         this.props.history.push("/");
+      } else {
+        this.props.history.push('/login');
+        this.props.clearUser();
       }
     });
   }
@@ -43,7 +46,7 @@ const mapStateToProps = state => ({
   isLoading: state.user.isLoading,
 });
 
-const RootWithAuth = withRouter(connect(mapStateToProps, { setUser })(Root));
+const RootWithAuth = withRouter(connect(mapStateToProps, { setUser, clearUser })(Root));
 
 ReactDOM.render(
   <Provider store={store}>
